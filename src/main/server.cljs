eac2e2e8145f266e05cd1250584bcf90418ea70a
Vale <valerauko@gmail.com>
2021-03-29 19:51:27 +0900

    Use http/listenAndServe for sanity

--- src/main/server.cljs
+++ src/main/server.cljs
@@ -2,38 +2,6 @@
   (:require ["https://deno.land/std@0.91.0/http/server.ts" :as http]
             [cljs.core.async :refer [go go-loop chan >! <! close!]]))
 
-;; the AsyncIterable -> core.async/chan implementation is based on
-;; @jjsullivan5196's gist
-;; https://gist.github.com/jjsullivan5196/0904057a2ec3eb080de5c4d6f45da630
-
-(defn onto-chan
-  [channel iterator]
-  (.. iterator
-      (next)
-      (then
-       (fn [item]
-         (if (.-done item)
-           (close! channel)
-           (go (when (>! channel [(.-value item) nil])
-                 (onto-chan channel iterator)))))
-       (fn [error]
-         (go (when (>! channel [nil error])
-               (onto-chan channel iterator)))))))
-
-(defn to-chan
-  [iterable]
-  (let [channel (chan)
-        iterator (js-invoke iterable js/Symbol.asyncIterator)]
-    (go (onto-chan channel iterator))
-    channel))
-
-(defn server-chan
-  [opts]
-  (-> opts
-      clj->js
-      http/serve
-      to-chan))
-
 (defn headers->map
   [headers]
   (reduce
@@ -65,13 +33,9 @@
   ([handler {:keys [host port]
              :or {host "0.0.0.0" port 8080}}]
    (println (str "Starting Deno HTTP server at " host ":" port))
-   (let [server (server-chan {:hostname host :port port})]
-     (go-loop []
-       (when-let [[req err] (<! server)]
-         (when req (->> req
-                        (req->ring)
-                        (handler)
-                        (ring->response)
-                        (.respond req)))
-         (when err (println "Error:" err))
-         (recur))))))
+   (http/listenAndServe (clj->js {:hostname host :port port})
+                        #(->> %
+                              req->ring
+                              handler
+                              ring->response
+                              (.respond %)))))
